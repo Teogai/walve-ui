2f27578f79beb9625ba2cd6a849e0f55ff74f930
PTS-Int <ek_kamol@hotmail.com>
2015-11-21 19:19:02 +0700

    finish dispenseController

---
+++ app/scripts/controllers/dispenseController.js
@@ -0,0 +1,62 @@
+'use strict';
+/**
+ * @ngdoc function
+ * @name walveApp.controller:AppointmentCtrl
+ * @description
+ * # AppointmentCtrl
+ * Controller of the walveApp
+ */
+angular.module('walveApp')
+  .controller('AppointmentCtrl', function($scope, $http) {
+  	$scope.laravelURL = '../../public/';
+
+  	$scope.findPatient = [];
+  	$scope.findPatient.patientID = '';
+  	$scope.dispense = [];
+  	$scope.dispense.medicineName = '';
+  	$scope.dispense.amount = '';
+	
+	//---------------------------------------Input to find Patient-----------------------------------//
+   	
+   	$scope.findPatient.submit = function(){
+  		$http({
+  			method: 'POST',
+			url: $scope.laravelURL + 'appointment',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			data: { 
+				patientID : $scope.findPatient.patientID
+			}
+		}).then(function successCallback(response) {
+			$scope.responseData = response.data;
+		  	// console.log(response.data);
+		  }, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
+  		};
+
+
+  	//---------------------------------------Add patient's basic data-----------------------------------------------//
+  	$scope.dispense.submit = function(){
+  		$http({
+  			method: 'POST',
+			url: $scope.laravelURL + 'appointment',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			data: { 
+				patientID : $scope.findPatient.patientID,
+				medicineName: $scope.dispense.medicineName,
+				amount: $scope.dispense.amount
+			}
+		}).then(function successCallback(response) {
+			
+			$scope.responseData = response.data;
+		  }, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
+  		};
+  });
