a0058fe3b78cf76f20317597501e0b04ebd38a44
PTS-Int <ek_kamol@hotmail.com>
2015-11-21 15:49:13 +0700

    add function makeDate + confirmData

--- app/scripts/controllers/appointmentController.js
+++ app/scripts/controllers/appointmentController.js
@@ -40,9 +40,16 @@ angular.module('walveApp')
 	  		},
 		]
 	}];
-  	$scope.makeData.selectedType = [];
+	
+	$scope.makeData.selectedType = [];
   	$scope.makeData.selectedTypeDetail = [];
-  	$scope.makeData.submit = function(){
+
+	$scope.confirmData = [];
+	$scope.confirmData.date = [];
+	
+	//---------------------------------------Input to find fastest date-----------------------------------//
+   	
+   	$scope.makeData.submit = function(){
   		$http({
   			method: 'POST',
 			url: $scope.laravelURL + 'appointment',
@@ -56,12 +63,44 @@ angular.module('walveApp')
 			}
 		}).then(function successCallback(response) {
 			$scope.isNextStep = true;
-		  	console.log(response.data);
+			$scope.responseData = response.data;
+		  	// console.log(response.data);
 		  }, function errorCallback(response) {
 		    // called asynchronously if an error occurs
 		    // or server returns response with an error status.
 		  });
   		};
 
-  	$scope.confirmData;
+  	//---------------------------------------Choose Date-----------------------------------------------//
+  	$scope.confirmData.submit = function(){
+  		$http({
+  			method: 'POST',
+			url: $scope.laravelURL + 'appointment',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			data: { 
+				number : $scope.makeData.number,
+				doctorID: $scope.confirmData.doctorID,
+				date: $scope.confirmData.date
+			}
+		}).then(function successCallback(response) {
+			$scope.responseData = response.data;
+			if ($scope.responseData.success) {
+				//show dialog like a confirmation card
+			}
+		  	// console.log(response.data);
+			else {
+				//"Sorry but it's full already; try selecting a new date"
+				// do this again
+			}		  	
+
+
+		  	}
+		  }, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
+  		};
+
   });
