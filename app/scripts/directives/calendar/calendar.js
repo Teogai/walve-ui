041319f8d89d2b364aa727d957ef87f58303911c
PTS-Int <ek_kamol@hotmail.com>
2015-11-23 11:37:21 +0700

    calendar always display current week

--- app/scripts/directives/calendar/calendar.js
+++ app/scripts/directives/calendar/calendar.js
@@ -11,12 +11,56 @@ angular.module('walveApp')
 
     var controller = ['$scope', function($scope) {
 
+      var date = new Date();
+      var curMon = getMonday(date);
+      var curMonloop = curMon;
+      var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
       $scope.calendar = $scope.data;
+      $scope.calendar.mon = curMonloop.getDate();
+      curMonloop.setDate(curMonloop.getDate() + 1);
+      $scope.calendar.tue = curMonloop.getDate();
+      curMonloop.setDate(curMonloop.getDate() + 1);
+      $scope.calendar.wed = curMonloop.getDate();
+      curMonloop.setDate(curMonloop.getDate() + 1);
+      $scope.calendar.thu = curMonloop.getDate();
+      curMonloop.setDate(curMonloop.getDate() + 1);
+      $scope.calendar.fri = curMonloop.getDate();
+
+      $scope.calendar.month = monthNames[curMon.getMonth()];
+      $scope.calendar.year = curMon.getFullYear();
       
       $scope.clicked = function () {
         console.log('clicked');
         $scope.calendar.month = 'December';
       };
+
+      $scope.previousMonth = function () {
+        console.log('lastMonth');
+        // $scope.calendar.month = 'December';
+      };
+
+      $scope.previousWeek = function () {
+        console.log('lastWeek');
+        // $scope.calendar.month = 'December';
+      };
+
+      $scope.nextWeek = function () {
+        console.log('nextWeek');
+        // $scope.calendar.month = 'December';
+      };
+
+      $scope.clicked = function () {
+        console.log('nextMonth');
+        // $scope.calendar.month = 'December';
+      };
+
+      function getMonday(d) {
+        d = new Date(d);
+        var day = d.getDay(),
+        diff = d.getDate() - day + (day == 0 ? -6:1); // adjust when day is sunday
+        return new Date(d.setDate(diff));
+      }
 
     }];
 
